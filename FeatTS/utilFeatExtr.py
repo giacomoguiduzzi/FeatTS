119f65bd8df32bb7676de8b53cb886060f0b8ee3
Giacomo Guiduzzi <giacomo.guiduzzi@unimore.it>
2025-01-22 19:15:01 +0100

    Edits for the paper https://github.com/giacomoguiduzzi/Interpretable_PS-InSAR_Clustering

--- FeatTS/utilFeatExtr.py
+++ FeatTS/utilFeatExtr.py
@@ -1,4 +1,6 @@
 import random
+from datetime import datetime
+
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.feature_selection import VarianceThreshold
@@ -6,13 +8,11 @@ from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.preprocessing import MinMaxScaler
 from pyclustering.cluster.kmedoids import kmedoids
 import networkx as nx
-import multiprocessing as mp
 from tsfresh import extract_features
-from FeatTS import SLPA
-import time
+from featts import SLPA
 import numpy as np
 from itertools import combinations
-import os
+import memory_profiler
 
 matrixSym = []
 
@@ -62,7 +62,7 @@ def choose_and_exclude_indices_by_percentage(classes, percentage):
     return chosen_indices, excluded_indices
 
 
-def getDataframeAcc(appSeries,perc):
+def getDataframeAcc(appSeries, perc):
     listClassExtr = list(appSeries.drop_duplicates())
     series = appSeries
     allAccInd = []
@@ -80,10 +80,10 @@ def getDataframeAcc(appSeries,perc):
                 else:
                     listIndexNotAccepted.append(i)
                     allNotAccInd.append(i)
-    return list(sorted(allAccInd)),list(sorted(allNotAccInd))
+    return list(sorted(allAccInd)), list(sorted(allNotAccInd))
+
 
 def extractFeature(listOut, external_feat=None, n_jobs=1):
-
     features_filtered_direct = extract_features(listOut, column_id='id', column_sort='time', n_jobs=n_jobs)
     if external_feat is not None:
         features_filtered_direct = features_filtered_direct.join(external_feat)
@@ -110,17 +110,16 @@ def normalization_data(features_filtered_direct):
     X = sorted_data.values.reshape(-1, 1)
 
     # Perform K-Means clustering with k=2
-    kmeans = KMeans(n_clusters=2)
+    kmeans = KMeans(n_clusters=2, n_init="auto")
     kmeans.fit(X)
-    variance_useless_column = variance[:(list(kmeans.labels_).count(0)-1)].index
+    variance_useless_column = variance[:(list(kmeans.labels_).count(0) - 1)].index
     features_filtered_direct.drop(columns=variance_useless_column, inplace=True)
-
 
     return features_filtered_direct
 
 
-
 from concurrent.futures import ProcessPoolExecutor
+
 
 def getTabNonSym(setCluster, listId, n_jobs=1):
     w = len(listId)
@@ -138,7 +137,7 @@ def getTabNonSym(setCluster, listId, n_jobs=1):
             start = i * totRig
             end = start + totRig if i < n_jobs - 1 else len(listId)
             futures.append(executor.submit(getValueMatrix, start, listId, end - start, setCluster))
-        
+
         for future in futures:
             matrixCalcParal(future.result())
 
@@ -149,11 +148,12 @@ def getTabNonSym(setCluster, listId, n_jobs=1):
     return matrixSym
 
 
-def getValueMatrix(start,listId,totRig,listOfClust):
+def getValueMatrix(start, listId, totRig, listOfClust):
     try:
         dictOfValueIJ = []
         # Pre-calcola i risultati di numOfClusterPres per evitare chiamate ripetute
-        cluster_pres_cache = {listId[i+start]: numOfClusterPres(listOfClust, listId[i+start]) for i in range(totRig)}
+        cluster_pres_cache = {listId[i + start]: numOfClusterPres(listOfClust, listId[i + start]) for i in
+                              range(totRig)}
 
         for i in range(totRig):
             id_i = listId[i + start]
@@ -176,21 +176,18 @@ def getValueMatrix(start,listId,totRig,listOfClust):
         print(e)
 
 
-def getCluster(matrixsym,setCluster,numClust):
-
+def getCluster(matrixsym, setCluster, numClust):
     dictTotal = {}
     for x in setCluster:
         listOfDist = []
         for y in setCluster:
             if x != y:
-                dictSing = {"id":y,"distance":matrixsym[x][y]}
+                dictSing = {"id": y, "distance": matrixsym[x][y]}
                 listOfDist.append(dictSing)
         dictTotal[x] = listOfDist
 
-
-    idChoose = getInitialIndex(dictTotal,numClust)
+    idChoose = getInitialIndex(dictTotal, numClust)
     D = pairwise_distances(matrixsym, metric='correlation')
-
 
     kmedoids_instance = kmedoids(D, idChoose, tolerance=0.000001)
     kmedoids_instance.process()
@@ -202,7 +199,6 @@ def getCluster(matrixsym,setCluster,numClust):
         dictApp = {i: Cl[i]}
         dictClu.update(dictApp)
 
-
     listOfCommFind = []
 
     for label in dictClu:
@@ -211,23 +207,25 @@ def getCluster(matrixsym,setCluster,numClust):
             listOfCommFind.append(dictSing)
     return listOfCommFind
 
-def numOfClusterPres(setCluster,id):
+
+def numOfClusterPres(setCluster, id):
     countId = 0
     countTimes = 0
-    for i in range(0,len(setCluster)):
+    for i in range(0, len(setCluster)):
         if id in (setCluster[i]["list"]):
             countId += (setCluster[i]["weight"])
             countTimes += 1
     return countId, countTimes
 
-def numOfRipetitionCouple(id1,id2,setCluster):
+
+def numOfRipetitionCouple(id1, id2, setCluster):
     countId = 0
     countTimes = 0
-    for i in range(0,len(setCluster)):
+    for i in range(0, len(setCluster)):
         if id1 in (setCluster[i]["list"]) and id2 in (setCluster[i]["list"]):
             countId += setCluster[i]["weight"]
             countTimes += 1
-    return countId,countTimes
+    return countId, countTimes
 
 
 def listOfId(setCluster):
@@ -237,23 +235,23 @@ def listOfId(setCluster):
             listId.add(id)
     return list(listId)
 
-def createSet(listOfCommFind,clusterK):
+
+def createSet(listOfCommFind, clusterK):
     listOfCluster = []
-    for i in range(0,clusterK):
-        dictSing = {"cluster":[],"label":i}
+    for i in range(0, clusterK):
+        dictSing = {"cluster": [], "label": i}
         listOfCluster.append(dictSing)
     for value in listOfCommFind:
         listApp = listOfCluster[value["label"]]["cluster"]
         listApp.append(value["cluster"])
         listOfCluster.remove(listOfCluster[value["label"]])
-        dictSing = {"cluster":listApp,"label":value["label"]}
-        listOfCluster.insert(value["label"],dictSing)
+        dictSing = {"cluster": listApp, "label": value["label"]}
+        listOfCluster.insert(value["label"], dictSing)
 
     return listOfCluster
 
 
-def randomFeat(ris,numberFeatUse):
-
+def randomFeat(ris, numberFeatUse):
     ris = ris.dropna(subset=['p_value'])
 
     indexNames = ris[ris['relevant'] == True].index
@@ -261,53 +259,62 @@ def randomFeat(ris,numberFeatUse):
     ris.drop(indexNames, inplace=True)
     randomFeat = ris.sample(n=numberFeatUse)
 
-
     return randomFeat
 
+def profile_community_detection_train(features, features_filtered_direct, listOfId, threshold, clusterK, chooseAlgorithm):
+    with open(f"memory_profiler_{datetime.now().strftime('%d_%m_%Y_%H_%M')}.log", "w+") as log_file:
+        @memory_profiler.profile(
+            precision=2,
+            stream=log_file
+        )
+        def getCommunityDetectionTrain(features, features_filtered_direct, listOfId, threshold, clusterK, chooseAlgorithm):
+            dictOfInfo = {}
 
-def getCommunityDetectionTrain(features, features_filtered_direct, listOfId, threshold, clusterK, chooseAlgorithm):
-    dictOfInfo = {}
+            for feature in features:
+                G = nx.Graph()
+                H = nx.path_graph(listOfId)
+                G.add_nodes_from(H)
 
-    for feature in features:
-        G = nx.Graph()
-        H = nx.path_graph(listOfId)
-        G.add_nodes_from(H)
+                # Convert the feature column to a numpy array for efficient operations
+                values = features_filtered_direct[feature].to_numpy()
+                # Calculate the pairwise absolute differences using broadcasting
+                diff_matrix = np.abs(values[:, np.newaxis] - values)
+                # Extract the upper triangle of the matrix, excluding the diagonal
+                listOfDistance = diff_matrix[np.triu_indices(len(values), k=1)]
+                # Find the threshold index
+                threshold_index = int(len(listOfDistance) * threshold)
+                # Use numpy's partition to find the threshold value without full sorting
+                distanceMinAccept = np.partition(listOfDistance, threshold_index)[threshold_index]
 
-        # Convert the feature column to a numpy array for efficient operations
-        values = features_filtered_direct[feature].to_numpy()
-        # Calculate the pairwise absolute differences using broadcasting
-        diff_matrix = np.abs(values[:, np.newaxis] - values)
-        # Extract the upper triangle of the matrix, excluding the diagonal
-        listOfDistance = diff_matrix[np.triu_indices(len(values), k=1)]
-        # Find the threshold index
-        threshold_index = int(len(listOfDistance) * threshold)
-        # Use numpy's partition to find the threshold value without full sorting
-        distanceMinAccept = np.partition(listOfDistance, threshold_index)[threshold_index]
+                for i, j in combinations(range(len(listOfId)), 2):
+                    if diff_matrix[i, j] < distanceMinAccept:
+                        G.add_edge(i, j)
 
-        for i, j in combinations(range(len(listOfId)), 2):
-            if diff_matrix[i, j] < distanceMinAccept:
-                G.add_edge(i, j)
-        
-        try:
-            if list(chooseAlgorithm.keys())[0] == 'SLPA':
-                extrC = SLPA.find_communities(G, chooseAlgorithm['SLPA']['iteration'], chooseAlgorithm['SLPA']['radious'])
-                coms = []
-                for val in extrC:
-                    coms.append(frozenset(extrC[val]))
-            elif list(chooseAlgorithm.keys())[0] == 'kClique':
-                coms = list(nx.algorithms.community.k_clique_communities(G, chooseAlgorithm['SLPA']['trainClique']))
-            elif list(chooseAlgorithm.keys())[0] == 'Greedy':
-                coms = list(nx.algorithms.community.greedy_modularity_communities(G))
+                try:
+                    if list(chooseAlgorithm.keys())[0] == 'SLPA':
+                        extrC = SLPA.find_communities(G, chooseAlgorithm['SLPA']['iteration'],
+                                                      chooseAlgorithm['SLPA']['radious'])
+                        coms = []
+                        for val in extrC:
+                            coms.append(frozenset(extrC[val]))
+                    elif list(chooseAlgorithm.keys())[0] == 'kClique':
+                        coms = list(nx.algorithms.community.k_clique_communities(G, chooseAlgorithm['SLPA']['trainClique']))
+                    elif list(chooseAlgorithm.keys())[0] == 'Greedy':
+                        coms = list(nx.algorithms.community.greedy_modularity_communities(G))
 
-            if len(coms) > clusterK:
-                dictOfInfo[feature] = {"distance": distanceMinAccept, "cluster": coms, "weightFeat": clusterK / len(coms)}
-            else:
-                dictOfInfo[feature] = {"distance": distanceMinAccept, "cluster": coms, "weightFeat": len(coms) / clusterK}
-            
-        except Exception as e:
-            print(e)
-            pass
-    return dictOfInfo
+                    if len(coms) > clusterK:
+                        dictOfInfo[feature] = {"distance": distanceMinAccept, "cluster": coms,
+                                               "weightFeat": clusterK / len(coms)}
+                    else:
+                        dictOfInfo[feature] = {"distance": distanceMinAccept, "cluster": coms,
+                                               "weightFeat": len(coms) / clusterK}
+
+                except Exception as e:
+                    print(e)
+                    pass
+            return dictOfInfo
+
+        return getCommunityDetectionTrain(features, features_filtered_direct, listOfId, threshold, clusterK, chooseAlgorithm)
 
 
 def cleaning(df: pd.DataFrame) -> pd.DataFrame:
@@ -323,7 +330,8 @@ def cleaning(df: pd.DataFrame) -> pd.DataFrame:
     df = df[top_features]
     return df
 
-def max_num_in_list( list ):
+
+def max_num_in_list(list):
     max = list[0]
     index = 0
     for i in range(1, len(list)):
@@ -332,17 +340,18 @@ def max_num_in_list( list ):
             index = i
     return max, index
 
-def min_num_in_list( list ):
-    min = list[ 0 ]
+
+def min_num_in_list(list):
+    min = list[0]
     index = 0
-    for i in range(1,len(list)):
+    for i in range(1, len(list)):
         if list[i] < min:
             min = list[i]
             index = i
-    return min,index
+    return min, index
 
 
-def getInitialIndex(dictTotal,cluster):
+def getInitialIndex(dictTotal, cluster):
     listOfMinDist = list()
     listOfMinId = list()
     dictSumDistance = dict()
@@ -350,7 +359,7 @@ def getInitialIndex(dictTotal,cluster):
         somma = 0
         for singleDict in dictTotal[idX]:
             somma += singleDict["distance"]
-        dictSumDistance[idX]=somma
+        dictSumDistance[idX] = somma
         if len(listOfMinDist) < cluster:
             listOfMinDist.append(dictSumDistance[idX])
             listOfMinId.append(idX)
